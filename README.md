612b94515539a08399bc3cc30076fe221e692b1f
Robert Ely <robertely@gmail.com>
2015-06-16 03:36:37 +0000

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+Overview
+========
+
+This is an attempt to replace the "retro-pi install script" with chef.
+http://blog.petrockblock.com/2012/07/22/retropie-setup-an-initialization-script-for-retroarch-on-the-raspberry-pi/
+
---
+++ cookbooks/emulationstation/metadata.rb
@@ -0,0 +1,8 @@
+name             'emulationstation'
+maintainer       'PizzaArcade'
+maintainer_email 'robert@pizzaarcade.co'
+license          'CC BY-SA 4.0'
+description      'Installs/Configures emulationstation'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
