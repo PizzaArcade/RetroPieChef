c72cf57a70973cbce32ba0785b5e787a85ef4733
Robert Ely <robertely@gmail.com>
2015-06-18 21:31:53 -0400

    Update kickstart.sh

--- kickstart.sh
+++ kickstart.sh
@@ -2,4 +2,5 @@
 sudo apt-get install git-core chef
 sudo rm -rf /var/chef/RetroPieChef/
 sudo git clone https://github.com/PizzaArcade/RetroPieChef.git /var/chef/RetroPieChef
+sudo wget -O /etc/chef/solo.rb https://raw.githubusercontent.com/PizzaArcade/RetroPieChef/master/solo.rb
 sudo chef-solo -j https://raw.githubusercontent.com/PizzaArcade/RetroPieChef/master/retropi.json
