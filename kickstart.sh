787c82b53c6528bead87b405c2b3c158a278c077
Robert Ely <robertely@gmail.com>
2015-06-18 23:38:51 -0400

    tidy

--- kickstart.sh
+++ kickstart.sh
@@ -4,7 +4,7 @@ sudo rm -rf /var/chef/RetroPieChef/
 sudo git clone https://github.com/PizzaArcade/RetroPieChef.git /var/chef/RetroPieChef
 
 sudo cat <<"EOF" > /etc/chef/solo.rb
-node_name "retro-pie"
+node_name        "retro-pie"
 cookbook_path    "/var/chef/RetroPieChef/cookbooks"
 role_path        "/var/chef/RetroPieChef/roles"
 checksum_path    "/var/chef/checksums"
