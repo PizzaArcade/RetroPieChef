013d13743004365629dcfc996c8da9bae637835f
Robert Ely <robertely@gmail.com>
2015-06-18 21:43:27 -0400

    embed solo.rb

--- kickstart.sh
+++ kickstart.sh
@@ -2,5 +2,19 @@
 sudo apt-get install git-core chef
 sudo rm -rf /var/chef/RetroPieChef/
 sudo git clone https://github.com/PizzaArcade/RetroPieChef.git /var/chef/RetroPieChef
-sudo wget -O /etc/chef/solo.rb https://raw.githubusercontent.com/PizzaArcade/RetroPieChef/master/solo.rb
+
+cat <<"EOF" > /etc/yum.repos.d/shutterstock-monitoring.repo
+node_name "retro-pie"
+cookbook_path    "/var/chef/RetroPieChef/cookbooks"
+role_path        "/var/chef/RetroPieChef/roles"
+checksum_path    "/var/chef/checksums"
+file_backup_path "/var/chef/backup"
+file_cache_path  "/var/chef/cache"
+json_attribs nil
+lockfile nil
+log_level :info
+log_location STDOUT
+solo true
+EOF
+
 sudo chef-solo -j https://raw.githubusercontent.com/PizzaArcade/RetroPieChef/master/retropi.json
